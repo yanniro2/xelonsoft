8e84fffee2d11c4c93678bedd9edac3729fa1df5
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-09-27 17:58:41 +0530

    header and footer is done

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#ffcd44", // Primary Color
+        "primary-dark": "#000000", // Darkest Shade
+        "primary-darker": "#0d0e0f", // Darker Shade
+        "dark-shade": "#1a1d1f", // Dark Shade
+        "primary-medium-shade": "#272c2f", // Medium Shade
+      },
+    },
+  },
+  plugins: [],
+};
